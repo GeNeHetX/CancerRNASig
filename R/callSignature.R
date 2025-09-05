36d2cb1af760f0e54272da4af5e0270f49b8c47a
Lucie GOMES <c.luciegomes@gmail.com>
2025-09-05 16:00:57 +0200

    add gempred biopsy in call function

--- R/callSignature.R
+++ R/callSignature.R
@@ -2,9 +2,9 @@
 #' 
 #' @param newexp gene expression matrix/dataframe, sample in columns, gene in rows
 #' @param geneSymbols gene symbols, a vector of same length as the number of rows in newex
-#' @param signature signature to apply, can be Gempred, Puleo, McpCount or Purist
-#' @param toNorm normlisation to apply if needed, can be none(by default), uq or vst 
-#' @param toScale scale, can be raw, sc=sample center, gc=gene center, gsc=gene scale center, ssc=sample scale center (none by default)
+#' @param signature signature to apply, can be Gempred, GempredBiopsy, Puleo, McpCount or Purist
+#' @param toNorm normlisation to apply if needed, can be none (by default), uq or vst 
+#' @param toScale scale to apply if needed, can be none (by default), sc=sample center, gc=gene center, gsc=gene scale center, ssc=sample scale center
 #'
 #' @return a data frame with row names as colnames of newexp, columns are different according the signatures, can be signature score or/and signature conclusion
 #'
@@ -13,11 +13,13 @@
 #' @export
 
 
-callSignature = function (matrix, geneSymbols, signature = c("Gempred", "Puleo", "McpCount", "Purist"), toNorm="none", toScale="none"){
+callSignature = function (matrix, geneSymbols, signature = c("Gempred", "GempredBiopsy", "Puleo", "McpCount", "Purist"), toNorm="none", toScale="none"){
   
   # -- Input validation & coercions -------------------------------------------------
   if (!is.matrix(matrix))
     matrix <- as.matrix(matrix)
+  if (all(grepl("^ENSG", rownames(data))))
+    stop("Row names of input matrix should be ENSG ids.")
   if (!is.character(geneSymbols))
     stop("geneSymbols must be a character vector.")
 
@@ -28,12 +30,14 @@ callSignature = function (matrix, geneSymbols, signature = c("Gempred", "Puleo",
   # -- Normalisation ----------------------------------------------------------
   data <- switch(toNorm,
     uq   = { message("Launching UQ normalisation") ; qutils::UQnorm(matrix) },
-    vst  = { message("Launching VST normalisation"); .VSTnorm(matrix)       },
+    vst  = { message("Launching VST normalisation"); vst(matrix)       },
     none = { message("No normalisation applied")   ; matrix                 }
   )
 
   # -- Scaling -----------------------------------------------------------------
   data <- .qNormalize(data, toScale)
+
+  # -- GeneSymbol -----------------------------------------------------------------
 
   # -- Signatures ---------------------------------------------------
   res <- switch(sig,
@@ -51,33 +55,21 @@ callSignature = function (matrix, geneSymbols, signature = c("Gempred", "Puleo",
       message("Launching Purist prediction")
       .purist(data, geneSymbols)
     },
-    ## add estimate signature
+    ## add estimate signature 
+
+    #need ENSG or Gene Symbols
     Gempred = {
       message("Launching GemPred prediction")
       .gemPred(data, geneSymbols)
     },
-    ## add gempred_biopsi
+    ## add GempredBiopsy - need ENSG
+    GempredBiopsy = {
+      message("Launching GemPred Biopsy prediction")
+      .GemPred_biopsy(data, gnt="raw",pnt="raw")
+    },
     
-    stop("Unreachable state – unknown signature. Please choose from 'Gempred', 'Puleo', 'McpCount' or 'Purist'.")  # sécurité
+    stop("Unreachable state – unknown signature. Please choose from 'Gempred', 'GempredBiopsy', 'Puleo', 'McpCount' or 'Purist'.")  # sécurité
   )
   
   res
 }
-
-## ADD to qutils package like UQnorm function ?
-.VSTnorm = function(counts){
-    conditions <- character(0)
-    mid = as.integer(dim(counts)[2]/2)
-    conditions <- c(rep("A",mid),rep("B",dim(counts)[2]-mid))
-    metadata <- data.frame(condition = factor(conditions))
-    rownames(metadata) <- colnames(counts)
-    dds <- DESeqDataSetFromMatrix(
-      countData = counts,
-      colData = metadata,
-      design = ~ condition
-    )
-    dds <- DESeq(dds)
-    dds =  vst(dds)
-    vst = assay(dds)
-    return(vst)
-  }
